963528bf01d641134dadf4c85d0439c2aac5d3ef
dgrossman <dgrossman@xyxxl.com>
2017-11-09 15:20:42 -0800

    key needs to include source

--- reconsile/reconsile.py
+++ reconsile/reconsile.py
@@ -5,18 +5,17 @@ from collections import defaultdict
 def reconsile(long_hash_f):
     o_data = defaultdict(dict)
     l_data = pickle.load(open(long_hash_f,'rb'))
-    for key in l_data:
-        new_key = key.split('_')[0]
-        o_data[new_key]['face_vec'] = l_data[key]['face_vec']
-        o_data[new_key]['face_pic'] = l_data[key]['face_pic']
-        o_data[new_key]['label'] = l_data[key]['label']
-        o_data[new_key]['frame_pic'] = []
-        o_data[new_key]['videos'] = {}
-        for vid in l_data[key]['videos']:
-            vid_key = vid.split('_')[0]
-            temp=list(l_data[key]['videos'][vid])
+    for entity_name in l_data:
+        o_data[entity_name]['face_vec'] = l_data[entity_name]['face_vec']
+        o_data[entity_name]['face_pic'] = l_data[entity_name]['face_pic']
+        o_data[entity_name]['label'] = l_data[entity_name]['label']
+        o_data[entity_name]['frame_pic'] = []
+        o_data[entity_name]['videos'] = {}
+        for vid in l_data[entity_name]['videos']:
+            vid_file_hash = vid.split('_')[0]
+            temp=list(l_data[entity_name]['videos'][vid])
             temp.sort(key=lambda tup: tup[0])
-            o_data[new_key]['videos'][vid_key]= temp
+            o_data[entity_name]['videos'][vid_file_hash]= temp
     return o_data
 
 if __name__ == "__main__":
